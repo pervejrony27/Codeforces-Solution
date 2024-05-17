259a7b9b1d23ff640fdf966eb46ad2cd6b9f3368
MD.Pervej Ahmed <147268299+pervejrony27@users.noreply.github.com>
2024-05-17 10:01:59 +0600

    Bun Lover

---
+++ 1822C.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        long long n;
+        cin >> n;
+        cout << n*n+(n+1)*2 << endl;
+
+    }
+    return 0;
+}
+ 
